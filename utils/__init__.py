8497c67533928fd512fa447cf9c1fdb7e898a523
uzbekprogrammer <mahmudovabdurahim04@gmail.com>
2022-02-28 14:24:09 +0300

    init.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,6 +7,6 @@ http://t.me/BaDBoY_DeV
 My portfolio: http://github.com/uzbekprogrammer
 
 """
-vfrom . import db_api
+from . import db_api
 from . import misc
 from .notify_admins import on_startup_notify
